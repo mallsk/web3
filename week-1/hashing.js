b701421b607c73231cb165210052130e3efb5b26
mallsk <skollermallikarjun@gmail.com>
2024-08-09 18:44:40 +0530

    solving another problem

--- week-1/hashing.js
+++ week-1/hashing.js
@@ -5,3 +5,22 @@ const hash = crypto.createHash('sha256').update(input).digest('hex');
 console.log(hash);
 
 //Output: 55711980d50fd24989409a7a7d4e15fcd206556eec1245c94a6e60910dc4e41d
+
+function findPrefix(prefix){
+    let number = 0;
+    while(true)
+    {
+        let a = number.toString();
+        let hashing = crypto.createHash('sha256').update(a).digest('hex');
+        if(hashing.startsWith(prefix)){
+            return {number: a,hashing:hashing}
+        }
+        number++;
+    }
+  }
+  const result = findPrefix('00000');
+  console.log(`Input: ${result.number}`);
+  console.log(`Hashing: ${result.hashing}`);
+
+// Output : Input: 596138
+//          Hashing: 00000691457f4f0ce13e187b9ab4fda6d42c8647752909b8f71f9dbd8f6bd4ab
