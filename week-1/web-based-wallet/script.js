2df1fa2e17062d1db59c1dddcc7b037f707130b2
mallsk <skollermallikarjun@gmail.com>
2024-08-15 07:57:24 +0530

    completed

--- week-1/web-based-wallet/script.js
+++ week-1/web-based-wallet/script.js
@@ -1,7 +1,27 @@
-function generate()
-{
-    const result = bip39.generateMnemonic();
-    const a = document.getElementById('wordphrase');
-    a.innerHTML = result;
+let mnemonic="";
+let wallets=[];
+document.getElementById('generate').addEventListener('click',() => {
+    mnemonic = ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(16));
+    document.getElementById('word-phrase').textContent = mnemonic;
+});
+document.getElementById('add-wallet').addEventListener('click', () => {
+    if (!mnemonic) {
+        alert('Please generate a Word list first');
+        return;
+    }
+  
+    const hdNode = ethers.utils.HDNode.fromMnemonic(mnemonic);
+    const wallet = hdNode.derivePath(`m/44'/60'/0'/0/${wallets.length}`);
+    wallets.push(wallet);
+    displayWallets();
+});
+
+function displayWallets() {
+    const walletList = document.getElementById('wallet-list');
+    walletList.innerHTML = '';
+    wallets.forEach((wallet, index) => {
+        const walletInfo = document.createElement('p');
+        walletInfo.textContent = `Wallet ${index + 1}: ${wallet.address}`;
+        walletList.appendChild(walletInfo);
+    });
 }
-generate();
